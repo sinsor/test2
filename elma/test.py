35e53d34159a10f833043ee04e432b33a54d77ad
ersdem@Mac <email@Mac.com>
2018-07-18 10:04:18 +0200

    pytest added

--- elma/test.py
+++ elma/test.py
@@ -1,22 +1,6 @@
-#!/usr/bin/python
-
-"""
-var1 = 'testing python script in docker container triggered by jenkins'
-print(var1)
-
-var2 = 9
-var3 = 5
-
-print("{} + {} = {}".format(var2, var3, (var2+var3)))
-"""
-
-
 def func(x):
     return x + 1
 
 
 def test_answer():
     assert func(4) == 5
-
-
-exit(0)
